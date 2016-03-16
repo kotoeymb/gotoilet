b66bc867dffc724b683889c85c8932e5d6c16402
shwe sin may oo <shwesinmayoo@pcp.co.jp>
2016-03-16 13:57:27 +0630

    FormViewUI Sketch

--- O.House/Views/FormViewController.designer.cs
+++ O.House/Views/FormViewController.designer.cs
@@ -1,18 +1,50 @@
-﻿// WARNING
+// WARNING
 //
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
 using Foundation;
+using System.CodeDom.Compiler;
 
 namespace OHouse
 {
 	[Register ("FormViewController")]
 	partial class FormViewController
 	{
+		[Outlet]
+		UIKit.UIButton CancleButton { get; set; }
+
+		[Outlet]
+		UIKit.UITextField DesTextField { get; set; }
+
+		[Outlet]
+		UIKit.UITextField NameTextField { get; set; }
+
+		[Outlet]
+		UIKit.UIButton SaveButton { get; set; }
+		
 		void ReleaseDesignerOutlets ()
 		{
+			if (NameTextField != null) {
+				NameTextField.Dispose ();
+				NameTextField = null;
+			}
+
+			if (SaveButton != null) {
+				SaveButton.Dispose ();
+				SaveButton = null;
+			}
+
+			if (CancleButton != null) {
+				CancleButton.Dispose ();
+				CancleButton = null;
+			}
+
+			if (DesTextField != null) {
+				DesTextField.Dispose ();
+				DesTextField = null;
+			}
 		}
 	}
 }
